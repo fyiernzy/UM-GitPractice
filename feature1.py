b586669f21c5f8967b331d8327ca53c169ab091e
fyiernzy <fyiernzy@gmail.com>
2023-05-28 18:08:32 +0800

    Add a simple for loop

--- feature1.py
+++ feature1.py
@@ -2,3 +2,7 @@ print("This is the first feature.")
 print("This is the second line, testing branch functionality.")
 
 print("Add new line in branch ZhiYang, testing for merge branch.")
+
+for i in range(5):
+    print("Hello world")
+    
